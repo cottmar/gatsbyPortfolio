b190cb473b0da847b2ab3084812faeb24d1a5201
cara ottmar <cara.ottmar@gmail.com>
2018-10-02 22:26:54 -0700

    working on react background image

--- gatsby-site/src/components/About/links.js
+++ gatsby-site/src/components/About/links.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Links extends Component {
   render() {
@@ -8,17 +8,17 @@ class Links extends Component {
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous"/>
         <ul className="smicons">
           <li className="icon">
-          <a href="https://github.com/cottmar" target="_blank">
+          <a href="https://github.com/cottmar" target="_blank" rel="noopener noreferrer">
             <i className="fa fa-github"></i>
           </a>
           </li>
           <li className="icon">
-            <a href="mailto:cara.ottmar@gmail.com" target="_blank">
+            <a href="mailto:cara.ottmar@gmail.com" target="_blank" rel="noopener noreferrer">
               <i className="fa fa-envelope"></i>
             </a>
           </li>
           <li className="icon">
-            <a href="https://www.linkedin.com/in/cara-ottmar/" target="_blank">
+            <a href="https://www.linkedin.com/in/cara-ottmar/" target="_blank" rel="noopener noreferrer">
               <i className="fa fa-linkedin"></i>
             </a>
           </li>
