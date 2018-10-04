8621c0987374c2be5d13aafb26a8abbe13e20e30
cara ottmar <cara.ottmar@gmail.com>
2018-10-03 21:30:19 -0700

    added work from index into background but need to undo. reading docs for guidance.

--- gatsby-site/src/components/backgroundImage/backgroundImage.js
+++ gatsby-site/src/components/backgroundImage/backgroundImage.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import forest from '../../images/forest/forest.jpg';
+import AboutMe from '../About/aboutme';
+import Links from '../About/links';
 
 class BackgroundImage extends React.Component{
   render() {
     return (
     <div className="backgroundImage">
       <img src={forest} alt="forest" width="100%" height="auto"/>
+       <div className="main">
+        <h5>
+          Hi, I'm <em className="bold">Cara Ottmar</em>
+        </h5>
+        <h3>
+          I am a Full-Stack Software Developer (MERN) with a background working in the financial industry. <br></br>
+          I have a passion for collaboration, innovation, and efficiency. <br></br>
+          I love how technology brings these three passions together to help make the world a better place. 
+        </h3>
+      <div className="aside">
+        <div className="top">
+          <AboutMe/>
+        </div>
+        <div className="bottom">
+          <Links />
+        </div>
+      </div>
+      </div>
     </div>
     )
   }
--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,30 +1,10 @@
 import React from 'react';
-import AboutMe from '../components/About/aboutme';
 import BackgroundImage from '../components/backgroundImage/backgroundImage';
-import Links from '../components/About/links';
 import '../pages/style.css';
 
 const IndexPage = () => (
   <div className="index">
   <BackgroundImage/>  
-      <div className="main">
-        <h5>
-          Hi, I'm <em className="bold">Cara Ottmar</em>
-        </h5>
-        <h3>
-         I am a Full-Stack Software Developer (MERN) with a background working in the financial industry. <br></br>
-         I have a passion for collaboration, innovation, and efficiency. <br></br>
-         I love how technology brings these three passions together to help make the world a better place. 
-        </h3>
-      </div>
-      <div className="aside">
-        <div className="top">
-          <AboutMe/>
-        </div>
-        <div className="bottom">
-          <Links />
-        </div>
-      </div>
-    </div>
+  </div>
 )
 export default IndexPage
