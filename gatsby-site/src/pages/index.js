3138011c0d46a8f269c3e6a005d1e2483305649c
cara ottmar <cara.ottmar@gmail.com>
2018-10-06 23:33:14 -0700

    struck out with react-native image but do have image rendering from index.js

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import BackgroundImage from '../components/backgroundImage/backgroundImage';
+import AboutMe from '../components/About/aboutme';
+import Links from '../components/About/links';
+import forest from '../images/forest/forest.jpg';
 import '../pages/style.css';
 
 const IndexPage = () => (
+  <div id="backgroundImage">
+    <img src={forest} alt="forest" width="100%"/>
+    {/* {this.props.children} */}
   <div className="index">
-  <BackgroundImage/>  
+  <div className="main">
+        <h5>
+          Hi, I'm <em className="bold">Cara Ottmar</em>
+        </h5>
+        <h3>
+          I am a Full-Stack Software Developer (MERN) with a background working in the financial industry. <br></br>
+          I have a passion for collaboration, innovation, and efficiency. <br></br>
+          I love how technology brings these three passions together to help make the world a better place. 
+        </h3>
+      <div className="aside">
+        <div className="top">
+          <AboutMe/>
+        </div>
+        <div className="bottom">
+          <Links />
+        </div>
+      </div>
+      </div>
+      </div>
   </div>
 )
+
+// const styles = StyleSheet.create({
+//   backgroundImage: {
+//     flex: 1,
+//     width: null,
+//     height: null,
+//     resizeMode: 'cover'
+//   }
+// });
+
 export default IndexPage
