69ac3602ae71f65352be0f44796714aa65f7b32f
cara ottmar <cara.ottmar@gmail.com>
2018-10-11 16:08:36 -0700

    working on one last dependency that is not installed

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -5,26 +5,26 @@ import Links from '../components/About/links';
 import '../pages/style.css';
 
 const IndexPage = () => (
-  <div className="main">
-  {/* <img src={forest} alt="forest" width="100%"/> */}
   <div className="index">
-        <h5>
-          Hi, I'm <em className="bold">Cara Ottmar</em>
-        </h5>
-        <h3>
-          I am a Full-Stack Software Developer (MERN) with a background working in the financial industry. <br></br>
-          I have a passion for collaboration, innovation, and efficiency. <br></br>
-          I love how technology brings these three passions together to help make the world a better place. 
-        </h3>
-      <div className="aside">
-        <div className="top">
-          <AboutMe/>
+    <div className="main">
+  {/* <img src={forest} alt="forest" width="100%"/> */}
+          <h5>
+            Hi, I'm <em className="bold">Cara Ottmar</em>
+          </h5>
+          <h3>
+            I am a Full-Stack Software Developer (MERN) with a background working in the financial industry. <br></br>
+            I have a passion for collaboration, innovation, and efficiency. <br></br>
+            I love how technology brings these three passions together to help make the world a better place. 
+          </h3>
+        <div className="aside">
+          <div className="top">
+            <AboutMe/>
+          </div>
+          <div className="bottom">
+            <Links />
+          </div>
         </div>
-        <div className="bottom">
-          <Links />
         </div>
-      </div>
-      </div>
   </div>
 )
 
